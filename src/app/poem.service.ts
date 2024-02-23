57622e411c5e28f90b78a293f0f9b6bfdbacb86e
Pelpav <elpav2022@gmail.com>
2024-02-23 14:35:26 +0000

    ERR HTTP2

--- src/app/poem.service.ts
+++ src/app/poem.service.ts
@@ -20,5 +20,17 @@ export class PoemService {
     };
     return this.http.get<Poem[]>('https://kikipavlova.000webhostapp.com/poems.php', httpOptions);
   }
+
+  getPoemById(id: number): Observable<Poem> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': '',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    return this.http.get<Poem>(`https://kikipavlova.000webhostapp.com/poems.php?id=${id}`, httpOptions);
+  }
+  
   
 }
--- src/app/poeme/poeme.component.ts
+++ src/app/poeme/poeme.component.ts
@@ -1,21 +1,25 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { PoemService } from '../poem.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Poem } from '../poem';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-poeme',
   standalone: true,
-  imports: [HttpClientModule, NgFor],
+  imports: [HttpClientModule, NgFor, NgIf],
   providers: [HttpClient, PoemService],
   templateUrl: './poeme.component.html',
   styleUrl: './poeme.component.scss'
 })
 export class PoemeComponent {
   poems: Poem[] = [];
+  selectedPoem: Poem | null;
 
-  constructor(private poemService: PoemService) { }
+  constructor(private poemService: PoemService) {
+    this.selectedPoem = null;
+   }
 
   ngOnInit(): void {
     this.poemService.getAllPoems().subscribe(
@@ -26,5 +30,28 @@ export class PoemeComponent {
         console.error('Erreur lors de la récupération des poèmes : ', error);
       }
     );
+  }
+
+  
+
+openPoem(id: number) {
+  this.poemService.getPoemById(id).subscribe(poem => {
+    this.selectedPoem = poem;
+  });
+}
+
+closePoem() {
+  this.selectedPoem = null;
+}
+
+}
+
+@Pipe({ name: 'formatLineBreaks' })
+export class FormatLineBreaksPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(value: string): SafeHtml {
+    const textWithBreaks = value.replace(/\n/g, '<br/>');
+    return this.sanitizer.bypassSecurityTrustHtml(textWithBreaks);
   }
 }
