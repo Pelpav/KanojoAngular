b932f3d263486285b8aedcbd56351ffb3970df46
Pelpav <elpav2022@gmail.com>
2024-02-27 10:39:13 +0000

    Add poeme final

--- src/app/addpoeme/addpoeme.component.ts
+++ src/app/addpoeme/addpoeme.component.ts
@@ -3,6 +3,8 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { HttpClient, HttpClientModule, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Poem } from '../poem';
+import { Router } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 
 @Component({
@@ -11,46 +13,44 @@ import { Poem } from '../poem';
     templateUrl: './addpoeme.component.html',
     styleUrl: './addpoeme.component.scss',
     imports: [NavbarComponent, ReactiveFormsModule, HttpClientModule], 
-    providers: [HttpClient, FormBuilder]
+    providers: [HttpClient, FormBuilder, RouterLink, RouterModule]
 })
 export class AddpoemeComponent{
     poemeForm!: FormGroup;
-    constructor (private http: HttpClient, private fb: FormBuilder){
+    constructor (private http: HttpClient, private fb: FormBuilder, private router: Router){
         this.poemeForm = new FormGroup({
             title: new FormControl('', Validators.required),
             content: new FormControl('', Validators.required),
         })
     }
     
-    ajouterPoeme(poem: Poem) {
+    ajouterPoeme(title: string, content: string) {
         const endpoint = 'https://kikipavlova.000webhostapp.com/poems.php'; // Remplacez par l'URL de votre endpoint
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'Content-Type':  'application/json',
-              'Authorization': '',
-              'Access-Control-Allow-Origin': '*'
-            })
-          };
+
+        const headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        headers.append('Access-Control-Allow-Origin', '*');
+
+
+        const formData = new FormData();
+            formData.append('title', title);
+            formData.append('content', content);
     
-        const body = {"id": poem.id, "title": poem.title, "content": poem.content, "created_at": poem.created_at };
-    
-        this.http.post(endpoint, body, httpOptions).subscribe(
+        this.http.post(endpoint, formData, {headers: headers, responseType: 'json',}).subscribe(
           response => {
             console.log('Poème ajouté avec succès :', response);
-            // Faites ce que vous voulez après l'ajout réussi
+            window.alert('Poème ajouté avec succès !');
+            this.router.navigate(['/poemes']); // Rediriger
           },
           error => {
             console.error('Erreur lors de l\'ajout du poème :', error);
-            // Gérez l'erreur en conséquence
+            window.alert('Erreur lors de l\'ajout du poème.');
           }
         );
     }
     
 
     onSubmit() {
-        const data = this.poemeForm.getRawValue();
-    
-        console.log(data);
         
         if (this.poemeForm.valid) {
             const titleControl = this.poemeForm.get('title');
@@ -63,7 +63,7 @@ export class AddpoemeComponent{
                     id: 0,
                     created_at: null,
                 };
-                this.ajouterPoeme(poem);
+                this.ajouterPoeme(poem.title, poem.content);
             }
         } else {
             console.error('Le formulaire est invalide.');
